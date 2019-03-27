d768970ba47f2c8550375855eeb808998a878543
Aone <quanyiz@stanford.edu>
2019-03-27 15:17:07 -0700

    Update bam2bigwig.sh

--- bam2bigwig.sh
+++ bam2bigwig.sh
@@ -3,11 +3,13 @@
 #create bed from bam, requires bedtools bamToBed
 bamToBed -i $1 -split > accepted_hits.bed
 
-#create plus and minus strand bedgraph
-cat accepted_hits.bed | sort -k1,1 | bedItemOverlapCount hg19 -chromSize=$len_hg19 stdin | sort -k1,1 -k2,2n > accepted_hits.bedGraph
+curl -s ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/chromInfo.txt.gz | gunzip -c > hg19_len
 
-bedGraphToBigWig accepted_hits.bedGraph $len_hg19 $1.bw
+#create plus and minus strand bedgraph
+cat accepted_hits.bed | sort -k1,1 | bedItemOverlapCount hg19 -chromSize=hg19_len stdin | sort -k1,1 -k2,2n > accepted_hits.bedGraph
+
+bedGraphToBigWig accepted_hits.bedGraph hg19_len $1.bw
 
 #removing intermediery files
-rm accepted_hits.bed
+rm accepted_hits.bed hg19_len
 rm accepted_hits.bedGraph
