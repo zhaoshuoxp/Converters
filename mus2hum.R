95ca1df01d81e026da350660001e1ed152cf199f
Aone <quanyiz@stanford.edu>
2020-07-17 11:06:46 -0700

    Create mus2hum.R

---
+++ mus2hum.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+#####################################
+# Basic function to convert mouse to human gene names
+convertMouseGeneList <- function(x){
+ 
+require("biomaRt")
+human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+ 
+genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=T)
+#print(genesV2)
+humanx <- unique(genesV2[, 2])
+
+return(genesV2)
+}
+
+# Basic function to convert human to mouse gene names
+convertHumanGeneList <- function(x){
+ 
+require("biomaRt")
+human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+ 
+genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+ 
+humanx <- unique(genesV2[, 2])
+
+return(genesV2)
+}
+ 
+
+
+options<-commandArgs(trailingOnly = T)
+dat<-read.table(file=options[1],header=F)
+out_name<-unlist(strsplit(options[1],'.',fixed=T))[1]
+
+dat$V1->mus
+convertMouseGeneList(mus)->hum
+
+write.table(hum, file=paste(out_name, 'hum.txt', sep='2'), row.names=F, sep="\t", quote=F, col.names=T)
