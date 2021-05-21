2e876c8f7fe18b44418a5dbccfd10bc9ce15cd1c
Aone <quanyiz@stanford.edu>
2021-05-21 08:45:28 +0800

    Update mus2hum.R

--- mus2hum.R
+++ mus2hum.R
@@ -29,12 +29,25 @@ return(genesV2)
 }
  
 
+mus2rm <- function(x){
+	
+require("biomaRt")
+monkey = useMart("ensembl", dataset = "mmulatta_gene_ensembl")
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+	
+genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = monkey, uniqueRows=T)
+#print(genesV2)
+humanx <- unique(genesV2[, 2])
+	
+return(genesV2)
+}
+
 
 options<-commandArgs(trailingOnly = T)
 dat<-read.table(file=options[1],header=F)
 out_name<-unlist(strsplit(options[1],'.',fixed=T))[1]
 
 dat$V1->mus
-convertMouseGeneList(mus)->hum
-
-write.table(hum, file=paste(out_name, 'hum.txt', sep='2'), row.names=F, sep="\t", quote=F, col.names=T)
+#convertMouseGeneList(mus)->hum
+mus2rm(mus)->rm
+write.table(rm, file=paste(out_name, 'hum.txt', sep='2'), row.names=F, sep="\t", quote=F, col.names=T)
